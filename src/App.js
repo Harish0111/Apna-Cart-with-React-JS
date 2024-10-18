06db79dd3da1a93b8750a834c552981347512e20
Harish Dapakara <harish.dapakara@expleogroup.com>
2024-10-18 16:01:03 +0530

    apna card app in react JS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
+import React,{useState} from 'react';
+
 
 function App() {
+  const products = [
+    {
+      price: 11999,
+      name : "Redmi Note 9",
+      quantity : 0,
+    },
+    {
+      price: 14999,
+      name : "Redmi Note 9 pro",
+      quantity : 0,
+    },
+    {
+      price: 25999,
+      name : "Google pixel 5",
+      quantity : 0,
+    }
+  ]
+  let [productList , setProductList] = useState(products)
+  let [totalAmount , setTotalAmount] = useState(0)
+
+    const increamentQuantity = (index) => {
+    let newProductList = [...productList]
+    let newTotalAmount = totalAmount;
+    newProductList[index].quantity++
+    newTotalAmount += newProductList[index].price
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  }
+  const decreamentQuantity = (index) => {
+    let newProductList = [...productList];
+    let newTotalAmount = totalAmount;
+    if(newProductList[index].quantity > 0){
+        newProductList[index].quantity-- ;
+        newTotalAmount -= newProductList[index].price
+    }
+    setTotalAmount(newTotalAmount);
+    setProductList(newProductList);
+  }
+
+  const resetQuantity=()=>{
+    let newProductList = [...productList];
+    newProductList.map((products) =>{
+      products.quantity = 0;
+    });
+    setProductList(newProductList);
+    setTotalAmount(0);
+  }
+
+  // const removeItem = (index) =>{
+  //   let newProductList = [...productList];
+  //   let newTotalAmount = totalAmount;
+  //   newTotalAmount -= newProductList[index].quantity * newProductList[index].price
+  //   newProductList.slice(index, 1);
+  //   setProductList(newProductList);
+  //   setTotalAmount(newTotalAmount);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <main className='container mt-5'>
+    <ProductList productList = {productList}
+    increamentQuantity = {increamentQuantity} 
+    decreamentQuantity= {decreamentQuantity} 
+    //removeItem = {removeItem}
+    />
+    </main>
+    
+    <Footer totalAmount= {totalAmount} resetQuantity={resetQuantity}/>
+
+    </>
   );
 }
 
